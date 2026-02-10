a753406536543a36b0871d66270557f54a303387
Melui <virginiadambrosio610@gmail.com>
2026-02-10 19:42:06 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <HashRouter basename={import.meta.env.BASE_URL}>
+        <HashRouter>
             <GameProvider>
                 <App />
             </GameProvider>
